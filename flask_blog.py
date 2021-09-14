34d469aa8003c7c005b2b0726a8d3af8ffc6a5a2
yaw twumasi <auxibe@gmail.com>
2021-09-14 16:31:29 +0100

    added

--- flask_blog.py
+++ flask_blog.py
@@ -1,8 +1,33 @@
+from enum import unique
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import LoginForm, RegistrationForm
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e133c32c00b2eb8f676e8695531d0ee3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    profile_pic = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def __repr__(self) -> str:
+        return f"User('{self.username}', '{self.email}', '{self.profile_pic}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default= datetime.utcnow)
+    content = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Post('{self.title}', '{self.created_at}')"
+    
+    
 
 posts = [
     {
