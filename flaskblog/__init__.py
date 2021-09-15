15ee78cdbd2fbe59d9246083506d35d8a0f13354
yaw twumasi <auxibe@gmail.com>
2021-09-15 15:22:14 +0100

    restructing of app into a package

---
+++ flaskblog/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from sqlalchemy.orm import backref
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e133c32c00b2eb8f676e8695531d0ee3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+from flaskblog import routes
