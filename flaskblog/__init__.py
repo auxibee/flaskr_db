ac9fdce77991a847e37b7b29511d91196b6d6f75
yaw twumasi <auxibe@gmail.com>
2021-09-15 18:23:36 +0100

    repackaging into a package

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from sqlalchemy.orm import backref
-
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,5 +8,5 @@ app.config['SECRET_KEY'] = 'e133c32c00b2eb8f676e8695531d0ee3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,17 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email,EqualTo
+from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
+from flaskblog.models import User
 
-class BaseForm(FlaskForm):
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
     email = StringField('Email', validators=[Email(), DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-
-
-class RegistrationForm(BaseForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-class LoginForm(BaseForm):
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username already exits')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('email has been used to register an account')
+
+    
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[Email(), DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Log in')
