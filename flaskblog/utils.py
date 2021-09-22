146ca9c23c9757e262c21b2b9af19c8f0e0f524b
auxibee <auxibee@gmail.com>
2021-09-22 22:24:20 +0000

    added save_picture and delete_profile_picture helper functions

---
+++ flaskblog/utils.py
@@ -0,0 +1,22 @@
+import secrets
+import os
+from PIL import Image
+from flaskblog import app
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, file_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + file_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def delete_profile_picture(picture_name):
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_name)
+    if os.path.exists(picture_path):
+        os.remove(picture_path)
