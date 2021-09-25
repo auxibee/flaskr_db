2f0178922ca2d21440868a644278155458a396f2
auxibee <auxibee@gmail.com>
2021-09-25 20:02:09 +0000

    created the create app function

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,33 +1,39 @@
-import os
+
 from flask import Flask
 from sqlalchemy.orm import backref
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from  flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'e133c32c00b2eb8f676e8695531d0ee3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['POST_PER_PAGE'] = 5
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'yaw217@gmail.com'
-app.config['MAIL_PASSWORD'] = "217317auxi"
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+mail = Mail()
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+
+
+def create_app(config=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
